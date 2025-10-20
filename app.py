babc26e29d8a27604bd435df4e2cd41ca827c500
papichulo666 <23308060610438@cetis61.edu.mx>
2025-10-20 09:02:07 -0500

    cvcvcv

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = 'sonic_secret_key_2025'
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():  # ← Cambié de login_page a login
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        password = request.form['password']
+        
+        if usuario == "sonic" and password == "123":
+            flash('Inicio de sesión exitoso')
+            return render_template('bienvenido.html', usuario=usuario)
+        else:
+            return render_template('login.html', error=True)
+    
+    return render_template('login.html')
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        usuario = request.form['usuario']
+        password = request.form['password']
+        password2 = request.form['password2']
+        
+        if password != password2:
+            flash('Las contraseñas no coinciden')
+            return redirect('/registro')
+    
+    return render_template('registro.html')
+
+@app.route('/animales')
+def animales():
+    return render_template('animales.html')
+
+@app.route('/vehiculos')
+def vehiculos():
+    return render_template('vehiculos.html')
+
+@app.route('/maravillas')
+def maravillas():
+    return render_template('maravillas.html')
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
